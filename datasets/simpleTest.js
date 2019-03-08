79b28d72d45b20bfa9569ac70f8f7fd71bd5e0b2
KKnapp <kknapp@sc.younglife.org>
2019-03-07 21:51:34 -0700

    cycles working and printing now

--- datasets/simpleTest.js
+++ datasets/simpleTest.js
@@ -1,1 +1,5 @@
-{"classes": [{"ApexClassId":"01p4100000MFexiAAD","Name":"ClassA", "ReferencedBy":{"classes":{"ClassB":[]}}},{"ApexClassId":"01p4100000MFexjAAD","Name":"ClassB", "ReferencedBy":{"classes":{"ClassA":[]}}}]}
+{"classes": [{"ApexClassId":"01p4100000MFexiAAD","Name":"ClassA", "ReferencedBy":{"classes":{"ClassB":[]}}},{"ApexClassId":"01p4100000MFexjAAD","Name":"ClassB", "ReferencedBy":{"classes":{"ClassA":[],"ClassC":[]}}},
+{"ApexClassId":"01p4100000MFexiAAE","Name":"ClassC", "ReferencedBy":{"classes":{"ClassB":[]}}},
+{"ApexClassId":"01p4100000MFexiAAF","Name":"ClassD", "ReferencedBy":{"classes":{"ClassE":[]}}},
+{"ApexClassId":"01p4100000MFexiAAG","Name":"ClassE", "ReferencedBy":{"classes":{"ClassD":[]}}}
+]}
--- js/common.js
+++ js/common.js
@@ -3,13 +3,14 @@ $(function () {
     //var maxEdges = 10;
 
     // create an array with nodes
-    var nodes = new vis.DataSet();
+    nodes = new vis.DataSet();
 
     // create an array with edges
     var edges = new vis.DataSet();
 
     // need a map to hold Classes and Id's
-    var mapClassId = {};
+    var mapNameToElement = {};
+    mapIdToElement = {};
 
     function parseNodesAndEdges(fileName){
 
@@ -30,38 +31,46 @@ $(function () {
                     //     AddNode(element);
                     // }
                     if (!element.Name.endsWith("Test")) {
-                        console.log(element.Name + ' ' + element.ApexClassId);
+                        //console.log(element.Name + ' ' + element.ApexClassId);
                         AddNode(element);
+                        mapNameToElement[element.Name] = element;
+                        mapIdToElement[element.ApexClassId] = element;   
                     }
                     //AddNode(element); // no filter.
                 }
     
                 // Second Pass. Create Edges b/c hopefully, all the classes are there.
-                for (let index = 0; index < Math.min(maxNodes, data.classes.length); index++) {
-                    const element = data.classes[index];
+                for (var el in mapIdToElement) {
+                    const element = mapIdToElement[el];                   
                     for (var key in element.ReferencedBy.classes) {
-                        AddEdge(mapClassId[key], element.ApexClassId);
+                        AddEdge(mapNameToElement[key].ApexClassId, element.ApexClassId);                        
                     }
                 }
-                // data.nodes.forEach(function(n){
-                //     for (var key in element.ReferencedBy.classes) {
-                //         AddEdge(mapClassId[key], element.ApexClassId);
-                //     }
-                // AddEdge(mapClassId[key], element.ApexClassId);
-                // })
             }
             });
+
     }
 
     function AddNode(codescan_element) {
         nodes.add({ id: codescan_element.ApexClassId, label: codescan_element.Name });
-        mapClassId[codescan_element.Name] = codescan_element.ApexClassId;
     }
 
     function AddEdge(fromClass, toClass) {
         if (fromClass == undefined || toClass == undefined)
             return;
         edges.add({ from: fromClass, to: toClass, arrows: 'to' });
+    }
+
+    function CyclesToHTMLString(cyc){
+        retval = "";
+        for (let index = 0; index < cyc.length; index++) {
+            for (let j = 0; j < cyc[index].length; j++){
+                retval += mapIdToElement[cyc[index][j]].Name;
+                retval += ",";
+            }
+            retval += "<br/>";
+        }
+        return retval;
     }
 
     // graph stuff after this
@@ -78,7 +87,9 @@ $(function () {
         });
 
         console.log("Cycles:");
-        console.log(graphlib.alg.findCycles(g));                                             
+        console.log(graphlib.alg.findCycles(g));    
+        $('#numCycles').html(CyclesToHTMLString(graphlib.alg.findCycles(g)));
+                                         
     }    
 
     //parseNodesAndEdges("datasets/SF Code Scan -INT.js");
@@ -113,8 +124,6 @@ $(function () {
 
     // initialize your network!
     var network = new vis.Network(container, data, options);
-
-
 
     evalGraph(data);
 
