8b94d93509347e787f6d6c93fd20b614a8139fab
kknapp <kenny@knapptech.com>
2019-03-19 23:31:47 -0600

    better table filters

--- js/tableHelper.js
+++ js/tableHelper.js
@@ -1,16 +1,54 @@
 $( function() {
     // DataTables setup
     var table = $('#classTable').DataTable( {
+        paging : true,
         data: fileData.classes,
         columns: [
-            { data: "ApexClassId", visible:false },
+            { data: null , defaultContent: '', orderable: false, className: 'select-checkbox' },
+            { data: "ApexClassId", visible:false, orderable: false },
             { title: "Name", data: "Name" },
             { title: "IsReferenced", data: "IsReferenced"}
-        ]
+        ],
+        order: [[ 2, "asc" ]],
+        lengthMenu: [[10, 50, 100, -1], [10, 50, 100, "All"]],        
+        select: {
+            style:    'os',
+            selector: 'td:first-child'
+        },
+        buttons: [
+            'selectAll',
+            'selectNone'
+        ],
+        language: {
+            buttons: {
+                selectAll: "Select all items",
+                selectNone: "Select none"
+            }
+        },
+        initComplete: function () {
+            this.api().columns().every( function () {
+                var column = this;
+                var select = $('<select><option value=""></option></select>')
+                    .appendTo( $(column.footer()).empty() )
+                    .on( 'change', function () {
+                        var val = $.fn.dataTable.util.escapeRegex(
+                            $(this).val()
+                        );
+ 
+                        column
+                            .search( val ? '^'+val+'$' : '', true, false )
+                            .draw();
+                    } );
+ 
+                column.data().unique().sort().each( function ( d, j ) {
+                    select.append( '<option value="'+d+'">'+d+'</option>' )
+                } );
+            } );
+        }        
     } );
-    $('#classTable tbody').on( 'click', 'tr', function () {
-        $(this).toggleClass('selected');
-    } );
+    // $('#classTable tbody').on( 'click', 'tr', function () {
+    //     $(this).toggleClass('selected');
+    // } );
 
     // Send the Button Click to the Eval Function
     $('#evalNodes').click( function () {
